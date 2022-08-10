9b1172f99039707c6cc02fa8b1340b782d3ddfb3
Carlos Alognon <carlosalognon08@gmail.com>
2022-08-10 15:06:49 +0100

    Fileiput components added

---
+++ resources/js/Components/FileInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <label v-if="label" class="form-label">{{ label }}:</label>
+    <div class="form-input p-0 border-gray-400 rounded">
+      <input ref="file" type="file" :accept="accept" class="hidden" :class="{ error: error } + 'border-gray-200 rounded'" @change="change" @input="$emit('update:modelValue', $event.target.files[0])" />
+      <div v-if="!modelValue" class="p-2">
+        <button type="button" class="px-4 py-1 bg-gray-500 hover:bg-gray-700 rounded-sm text-xs font-medium text-white" @click="browse">
+          Browse
+        </button>
+      </div>
+      <div v-else class="flex items-center justify-between p-2">
+        <div class="flex-1 pr-1">
+          {{ modelValue.name }} <span class="text-gray-500 text-xs">({{ filesize(modelValue.size) }})</span>
+        </div>
+        <button type="button" class="px-4 py-1 bg-gray-500 hover:bg-gray-700 rounded-sm text-xs font-medium text-white" @click="remove">
+          Remove
+        </button>
+      </div>
+    </div>
+    <div v-if="error" class="form-error">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: File,
+    label: String,
+    accept: String,
+    error: String,
+  },
+  watch: {
+    modelValue(value) {
+      if (!value) {
+        this.$refs.file.value = ''
+      }
+    },
+  },
+  emits: ['update:modelValue'],
+  methods: {
+    filesize(size) {
+      var i = Math.floor(Math.log(size) / Math.log(1024))
+      return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i]
+    },
+    browse() {
+      this.$refs.file.click()
+    },
+    change(e) {
+    //   this.$emit('update:modelValue', e.target.files[0])
+      console.log(this.modelValue);
+    },
+    remove() {
+      this.$emit('update:modelValue', null)
+    },
+  },
+}
+</script>
