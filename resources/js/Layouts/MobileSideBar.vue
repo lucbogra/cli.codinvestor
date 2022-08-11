6cc8f0e3a2524cefbf0b882106c085c7dcb9ee06
Carlos Alognon <carlosalognon08@gmail.com>
2022-08-11 15:23:13 +0100

    Mobile

--- resources/js/Layouts/MobileSideBar.vue
+++ resources/js/Layouts/MobileSideBar.vue
@@ -22,7 +22,7 @@
                 <inertia-link v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-cyan-800 text-white' : 'text-cyan-100 hover:text-white hover:bg-cyan-600', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']" :aria-current="item.current ? 'page' : undefined">
                   <component :is="item.icon" class="mr-4 flex-shrink-0 h-6 w-6 text-cyan-200" aria-hidden="true" />
                   {{ item.name }}
-                </inertia-link>link
+                </inertia-link>
               </div>
               <div class="mt-6 pt-6">
                 <div class="px-2 space-y-1">
