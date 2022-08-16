d4602c0586f5cb7c946da24b3993242c61f79ee3
Carlos Alognon <carlosalognon08@gmail.com>
2022-08-16 16:41:11 +0100

    Duplicated added

---
+++ resources/js/Pages/Orders/Duplicated.vue
@@ -0,0 +1,146 @@
+<script setup>
+import { useForm, usePage } from '@inertiajs/inertia-vue3';
+import { ref, computed, onMounted } from 'vue';
+const props = defineProps({
+  orders: Array,
+})
+
+const filters = ref({
+  all: { value: '', keys: ['customer_name', 'phone', 'country', 'city', 'product_name', 'price', 'website', 'created_at'] }
+})
+
+onMounted(() =>{
+    console.log(props.orders);
+});
+
+const selectedOrders = ref([])
+const checked = ref(false)
+const indeterminate = computed(() => selectedOrders.value.length > 0 && selectedOrders.value.length < props.orders.length)
+
+const currentPage = ref(1)
+const totalPages = ref(0)
+const tag = ref()
+const form = useForm({
+  agents: [],
+  rows: ''
+})
+
+
+</script>
+
+<template>
+  <div>
+    <div class="mt-8 flex flex-col">
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div
+            class="relative overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg divide-y divide-gray-300">
+            <div class="flex justify-between items-center py-2 pr-1 pl-3 bg-gray-50">
+              <h3 class="text-lg leading-6 font-medium text-gray-900">Orders</h3>
+              <input class="form-input w-full lg:w-1/4 rounded-full" placeholder="search..."
+                v-model="filters.all.value" />
+            </div>
+            <VTable :data="orders" :filters="filters" class="min-w-full table-fixed divide-y divide-gray-300"
+              :page-size="8" v-model:currentPage="currentPage" @totalPagesChanged="totalPages = $event">
+              <template #head>
+                <tr class="bg-gray-50">
+                  <th scope="col" class="relative w-12 px-6 sm:w-16 sm:px-8">
+                    <input type="checkbox"
+                      class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-cyan-600 focus:ring-cyan-500 sm:left-6"
+                      :checked="indeterminate || selectedOrders.length === orders.length" :indeterminate="indeterminate"
+                      @change="selectedOrders = $event.target.checked ? orders.map((p) => p.id) : []" />
+                  </th>
+                  <th class="py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">Date</th>
+                  <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Customer Name</th>
+                  <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Phone</th>
+                  <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Location</th>
+                  <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">sku</th>
+                  <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">price</th>
+                  <th class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">website</th>
+                </tr>
+              </template>
+              <template #body="{ rows }">
+                <tr v-for="row in rows" :key="row.id" class="divide-y divide-gray-200 bg-white hover:bg-gray-100 focus-within:bg-gray-100"
+                  :class="[selectedOrders.includes(row.id) && 'bg-gray-50']">
+                  <td class="relative w-12 px-6 sm:w-16 sm:px-8">
+                    <div v-if="selectedOrders.includes(row.id)" class="absolute inset-y-0 left-0 w-0.5 bg-cyan-600">
+                    </div>
+                    <input type="checkbox"
+                      class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-cyan-600 focus:ring-cyan-500 sm:left-6"
+                      :value="row.id" v-model="selectedOrders" />
+                  </td>
+                  <td
+                    :class="['whitespace-nowrap py-4 pr-3 text-sm font-medium', selectedOrders.includes(row.id) ? 'text-cyan-600' : 'text-gray-900']">
+                    {{ row.created_at }}</td>
+                  <td
+                    :class="['whitespace-nowrap px-3 py-4 text-sm', selectedOrders.includes(row.id) ? 'text-cyan-600' : ' text-gray-500']">
+                    {{ row.customer_name }}</td>
+                  <td
+                    :class="['whitespace-nowrap px-3 py-4 text-sm', selectedOrders.includes(row.id) ? 'text-cyan-600' : ' text-gray-500']">
+                    {{ row.phone }}</td>
+                  <td
+                    :class="['whitespace-nowrap px-3 py-4 text-sm', selectedOrders.includes(row.id) ? 'text-cyan-600' : ' text-gray-500']">
+                    {{ row.country + '-' + row.customer_city }}</td>
+                  <td
+                    :class="['whitespace-nowrap px-3 py-4 text-sm', selectedOrders.includes(row.id) ? 'text-cyan-600' : ' text-gray-900']">
+                    {{ row.product_name }}</td>
+                  <td
+                    :class="['whitespace-nowrap px-3 py-4 text-sm', selectedOrders.includes(row.id) ? 'text-cyan-600' : ' text-gray-500']">
+                    {{ row.price }}</td>
+                  <td
+                    :class="['whitespace-nowrap px-3 py-4 text-sm', selectedOrders.includes(row.id) ? 'text-cyan-600' : ' text-gray-500']">
+                    {{ row.website }}</td>
+                </tr>
+                <tr v-if="rows.length === 0">
+                 <td class="border-t px-6 py-4" colspan="4">No order found.</td>
+                </tr>
+              </template>
+            </VTable>
+
+            <VTPagination v-model:currentPage="currentPage" :total-pages="totalPages" :boundary-links="false"
+              :maxPageLinks="9" class="flex">
+              <template #firstPage class="flex">
+                <i class="fas fa-arrow-left" />
+              </template>
+
+              <template #lastPage>
+                <i class="fas fa-arrow-right" />
+              </template>
+
+              <template #next>
+                <i class="fas fa-chevron-right" />
+              </template>
+
+              <template #previous>
+                <i class="fas fa-chevron-left" />
+              </template>
+            </VTPagination>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+
+<style>
+    ul{
+        display: flex;
+        justify-content: center;
+        margin: 20px 0;
+    }
+     ul li {
+        padding: 8px 17px;
+     }
+
+     ul li{
+       border: 1px solid #ddd;
+     }
+
+     ul .active {
+        background-color: #1B2E51;
+        color: white;
+     }
+</style>
