47f62ec9695b990b2203ab68a7d458d99d847d5f
lucbogra <53279389+lucbogra@users.noreply.github.com>
2023-03-22 11:04:58 +0000

    demo video

--- resources/js/Pages/Orders/UploadForm.vue
+++ resources/js/Pages/Orders/UploadForm.vue
@@ -5,7 +5,9 @@
       <FileInput v-model="form.file" class="pr-6 pb-8 w-full lg:w-1/2" type="file" accept=".xlsx, .xls, .csv"
         label="file" />
       <LoadingButton :loading="form.processing" class="btn-primary ml-4" type="submit" :disabled="form.file === null">Import Orders</LoadingButton>
-      <a class="ml-4 text-primary-700 hover:text-primary-400 hidden sm:block" :href="demo_file"> To know the structure of the file to import, download the demo file</a>
+      <a class="ml-4 btn-white flex" :href="demo_file"><i class="fa-solid fa-download mr-1"></i> Download the demo file</a>
+      <a class="ml-4 btn-danger flex" href="https://youtu.be/W9_f1KSU0rc" target="_blank" ><i class="fa-brands fa-youtube fa-beat mr-1" ></i>Demo</a>
+
     </div>
     <div class="mb-2">
       <div class="form-error" v-for="(err, index) in form.errors" :key="index">{{ err }}</div>
