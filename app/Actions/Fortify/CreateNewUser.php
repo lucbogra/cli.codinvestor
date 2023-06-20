9db3fce491d6a3a28769a7cb58b85a756bd800e7
lucbogra <53279389+lucbogra@users.noreply.github.com>
2023-06-20 12:51:35 +0100

    show token

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,14 +22,14 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input)
     {
-      // dd($input['recaptcha_token']);
+      dd(config('services.recaptcha.secret_key'));
       $response = Http::asForm()->post("https://www.google.com/recaptcha/api/siteverify", [
         'secret' => config('services.recaptcha.secret_key'),
         'response' => $input['recaptcha_token'],
         'ip' => request()->ip(),
       ]);
 
-      // dd($response->json('success'));
+      dd($response->json('success'));
 
       $messages = [
         'required' => 'The :attribute field is required.',
